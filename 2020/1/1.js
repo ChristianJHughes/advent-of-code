ed61ca84a2d082a6f95f79b78e8ab8986b7f5f3a
Christian Hughes <chughes.mail@icloud.com>
2020-12-01 22:02:37 -0600

    Part 1 Refactor.

--- 2020/1/1.js
+++ 2020/1/1.js
@@ -5,7 +5,17 @@ var fileContent = fs.readFileSync(process.argv[2] || "input.txt", "utf8");
 const expenses = fileContent.split("\n").map((elem) => parseInt(elem));
 
 // Slowpoke function with naive n^2 search scope
-function naiveExpensesSearch() {
+function naiveExpensesSearchPart1() {
+  for (let i = 0; i < expenses.length; i++) {
+    for (let j = 0; j < expenses.length; j++) {
+      if (i != j && expenses[i] + expenses[j] == 2020) {
+        return expenses[i] * expenses[j];
+      }
+    }
+  }
+}
+
+function naiveExpensesSearchPart2() {
   for (let i = 0; i < expenses.length; i++) {
     for (let j = 0; j < expenses.length; j++) {
       for (let z = 0; z < expenses.length; z++) {
@@ -22,4 +32,5 @@ function naiveExpensesSearch() {
   }
 }
 
-console.log(naiveExpensesSearch());
+console.log(naiveExpensesSearchPart1());
+console.log(naiveExpensesSearchPart2());
